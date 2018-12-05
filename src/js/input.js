f12c592ec577a3b57757684dceef21b07ad90b69
luffa <petcharanon@gmail.com>
2018-12-05 15:48:38 +0700

    config

--- src/js/input.js
+++ src/js/input.js
@@ -1,10 +1,5 @@
 function inputScore(){
-<<<<<<< HEAD
-    //var input = $("#score").val();
-    return input;
-=======
    var input = $("#score").val();
    //console.log(input)
    return input;
->>>>>>> 0dea58ea579b66fad76b2bc315361e3e38d248ed
 }
